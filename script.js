9ccb972d2d4b6e7484d4dea0e7981aaa45927989
Maxim Ryabov <foba1995@gmail.com>
2020-03-17 02:00:54 +0400

    feat: finish task

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// header
 const menu = document.getElementById('menu');
 
 
@@ -5,6 +6,8 @@ menu.addEventListener('click', (event)=> {
     menu.querySelectorAll('a').forEach(el=>el.classList.remove('active'));
     event.target.classList.add('active');
 });
+
+// portfolio
 
 const tabs = document.getElementById('tabs');
 const random = document.getElementById('random');
@@ -28,6 +31,7 @@ random.addEventListener('click', (event)=>{
     event.target.classList.add('imgBorder')
 })
 
+// iphone click
 const vertical = document.getElementById('vertical');
 const horizontal = document.getElementById('horisontal');
 
@@ -48,4 +52,88 @@ horizontal.addEventListener('click', ()=>{
     else
     horScreen.style.display="block";
     countScreenHorizontal++
+});
+
+//slider
+let items = document.querySelectorAll('.sliderItem');
+
+let currentItem = 0;
+
+let isEnabled = true;
+
+function changeCurrentItem(n) {
+    currentItem = (n + items.length) % items.length;
+}
+
+function hideItem(direction) {
+    isEnabled = false;
+    items[currentItem].classList.add(direction);
+    items[currentItem].addEventListener('animationend', function(){
+        this.classList.remove('active', direction)
+    })
+}
+
+function showItem(direction) {
+    items[currentItem].classList.add('next', direction);
+    items[currentItem].addEventListener('animationend', function(){
+        this.classList.remove('next', direction);
+        this.classList.add('active');
+        isEnabled = true;
+    })
+}
+
+
+
+function previousItem(n) {
+    hideItem('to-right');
+    changeCurrentItem(n - 1)
+    showItem('from-left');
+}
+
+function nextItem(n) {
+    hideItem('to-left');
+    changeCurrentItem(n + 1)
+    showItem('from-right');
+}
+
+document.querySelector('.slider_left').addEventListener('click', function() {
+    if (isEnabled) {
+        previousItem(currentItem);
+    }
+    
+});
+
+document.querySelector('.slider_right').addEventListener('click', function() {
+    if (isEnabled) {
+        nextItem(currentItem);
+    }
+    
+});
+
+// forma
+const btn = document.getElementById('submit');
+const cls = document.getElementById('close');
+
+btn.addEventListener('click', ()=>{
+    let theme = document.getElementById('theme').value.toString();
+    let message = document.getElementById('textarea').value.toString();
+    if (theme == '') {
+        document.getElementById('resultTheme').innerText = 'Без темы';
+    }
+    else {
+        document.getElementById('resultTheme').innerText = 'Тема: ' + theme;
+    }
+    if (message == '') {
+        document.getElementById('resultMessage').innerText = 'Без описания';
+    }
+    else {
+        document.getElementById('resultMessage').innerText = 'Описание: ' + message;
+    }
+    document.getElementById('msg').classList.remove('hide');
+
+
+});
+
+cls.addEventListener('click', ()=>{
+    document.getElementById('msg').classList.add('hide');
 });
